2235b1a6d0b60debb69e4944d2eb5e028db2c488
sukhiboi <41sukhdevsingh@gmail.com>
2021-08-15 22:59:16 +0530

    Added List Vector Nil and HashMap types

--- impls/sukhiboi-js/reader.js
+++ impls/sukhiboi-js/reader.js
@@ -1,58 +1,71 @@
+const {List, Vector, Nil, HashMap, Symbol} = require("./types");
+
 class Reader {
-  constructor(tokens) {
-    this.tokens = [...tokens];
-    this.position = 0;
-  }
+    constructor(tokens) {
+        this.tokens = [...tokens];
+        this.position = 0;
+    }
 
-  peek() {
-    return this.tokens[this.position];
-  }
+    peek() {
+        return this.tokens[this.position];
+    }
 
-  next() {
-    const token = this.peek();
-    if (token) this.position++;
-    return token;
-  }
+    next() {
+        const token = this.peek();
+        if (token) this.position++;
+        return token;
+    }
 }
 
 const tokenize = str => {
-  const regex =
-    /[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"?|;.*|[^\s\[\]{}('"`,;)]*)/g;
-  const tokens = [];
-  while ((token = regex.exec(str)[1]) !== '')
-    if (token[0] !== ';') tokens.push(token);
-  return tokens;
+    const regex =
+        /[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"?|;.*|[^\s\[\]{}('"`,;)]*)/g;
+    const tokens = [];
+    while ((token = regex.exec(str)[1]) !== '')
+        if (token[0] !== ';') tokens.push(token);
+    return tokens;
 };
 
 const read_atom = token => {
-  if (token.match(/^-?[0-9]+$/)) return parseInt(token);
-  if (token.match(/^-?[0-9]+\.?[0-9]+$/)) return parseFloat(token);
-  return token;
+    if (token.match(/^-?[0-9]+$/)) return parseInt(token);
+    if (token.match(/^-?[0-9]+\.?[0-9]+$/)) return parseFloat(token);
+    return new Symbol(token);
 };
 
-const read_list = reader => {
-  const result = [];
-  while ((token = reader.peek()) !== ')') {
-    if (!token) throw "unbalanced";
-    result.push(read_form(reader));
+const read_seq = (reader, closing) => {
     reader.next();
-  }
-  return result;
-};
+    const result = [];
+    let token = reader.peek();
+    while (token !== closing) {
+        if (!token) throw 'unbalanced';
+        result.push(read_form(reader));
+        reader.next();
+        token = reader.peek();
+    }
+    return result;
+}
+
+const read_hashmap = reader => {
+    const seq = read_seq(reader, '}');
+    if(seq.length % 2 !== 0) throw 'unbalanced'
+    return seq;
+}
 
 const read_form = reader => {
-  const token = reader.peek();
-  if (token === '(') {
-    reader.next();
-    return read_list(reader);
-  }
-  return read_atom(token);
+    const token = reader.peek();
+    if (token === '(') return new List(read_seq(reader, ')'));
+    if (token === '[') return new Vector(read_seq(reader, ']'));
+    if (token === '{') return new HashMap(read_hashmap(reader));
+    if (token === 'true') return true;
+    if (token === 'false') return false;
+    if (token === 'nil') return new Nil();
+    return read_atom(token);
 };
 
 const read_str = str => {
-  if (str === '') return '';
-  const reader = new Reader(tokenize(str));
-  return read_form(reader);
+    if (str === '') return '';
+    const reader = new Reader(tokenize(str));
+    return read_form(reader);
 };
 
 module.exports = read_str;
---
+++ impls/sukhiboi-js/types.js
@@ -0,0 +1,66 @@
+class List {
+    constructor(seq) {
+        this.seq = seq;
+    }
+
+    isEmpty() {
+        return this.seq.length === 0;
+    }
+
+    toString() {
+        return `(${this.seq.map(form => form.toString()).join(' ')})`
+    }
+}
+
+class Vector {
+    constructor(seq) {
+        this.seq = seq;
+    }
+
+    isEmpty() {
+        return this.seq.length === 0;
+    }
+
+    toString() {
+        return `[${this.seq.map(form => form.toString()).join(' ')}]`
+    }
+}
+
+class Nil {
+    constructor() {
+    }
+
+    toString() {
+        return 'nil';
+    }
+}
+
+class HashMap {
+    constructor(seq) {
+        this.seq = seq;
+        this.hashmap = new Map();
+        for (let i = 0; i < seq.length; i += 2) {
+            this.hashmap.set(seq[i], seq[i + 1]);
+        }
+    }
+
+    isEmpty() {
+        return this.seq.length === 0;
+    }
+
+    toString() {
+        return `{${this.seq.map(form => form.toString()).join(' ')}}`
+    }
+}
+
+class Symbol {
+    constructor(symbol) {
+        this.symbol = symbol
+    }
+
+    toString() {
+        return `${this.symbol}`;
+    }
+}
+
+module.exports = {List, Vector, Nil, HashMap, Symbol};
