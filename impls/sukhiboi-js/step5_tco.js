cd93c47fe6573513b007aa1e893647108a40b614
sukhiboi <41sukhdevsingh@gmail.com>
2021-08-27 22:40:44 +0530

    updated let*

---
+++ impls/sukhiboi-js/step5_tco.js
@@ -0,0 +1,78 @@
+const readline = require('readline');
+const read_str = require('./reader');
+const pr_str = require('./printer');
+const {List, Symbol, Vector, HashMap, Nil, Func} = require("./types");
+const Env = require('./env');
+const CORE_ENV = require('./core');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const eval_ast = (ast, env) => {
+    if (ast instanceof List) return ast.map(form => EVAL(form, env))
+    if (ast instanceof Vector) return ast.map(form => EVAL(form, env))
+    if (ast instanceof HashMap) return ast.valueMap(form => EVAL(form, env))
+    if (ast instanceof Symbol) return env.get(ast);
+    return ast;
+}
+
+const READ = read_str;
+const EVAL = (ast, env) => {
+    while (true) {
+        if (!(ast instanceof List)) return eval_ast(ast, env);
+        if (ast instanceof List && ast.isEmpty()) return ast
+        if (ast instanceof List) {
+            switch (ast.seq[0].symbol) {
+                case 'def!':
+                    const value = EVAL(ast.seq[2], env);
+                    env.set(ast.seq[1], value)
+                    return value;
+                case 'let*':
+                    const localEnv = new Env(env);
+                    const bindingList = ast.seq[1].seq;
+                    for (let i = 0, j = i + 1; i < bindingList.length; i += 2) {
+                        let evaluatedValue = EVAL(bindingList[j], localEnv);
+                        localEnv.set(bindingList[i], evaluatedValue);
+                    }
+                    env = localEnv;
+                    ast = ast.seq[2];
+                    break;
+                case 'do':
+                    const [result] = EVAL(new List(ast.seq.slice(1)), env).seq.slice(-1);
+                    return result;
+                case 'if':
+                    const [condition, truthyValue, falsyValue] = ast.seq.slice(1);
+                    const evaluatedCondition = EVAL(condition, env);
+                    if (evaluatedCondition !== false && !(evaluatedCondition instanceof Nil)) return EVAL(truthyValue, env);
+                    return falsyValue ? EVAL(falsyValue, env) : new Nil();
+                case 'fn*':
+                    return new Func(function (...args) {
+                        const localEnv = new Env(env, ast.seq[1], args);
+                        return EVAL(ast.seq[2], localEnv);
+                    })
+            }
+            const [fnToCall, ...args] = eval_ast(ast, env).seq;
+            return fnToCall.apply(args);
+        }
+    }
+};
+const PRINT = pr_str;
+const rep = (str, env) => PRINT(EVAL(READ(str), env));
+
+
+const repl = env => {
+    rep('(def! not (fn* (a) (if a false true)))', env)
+    rl.question('user> ', input => {
+        try {
+            console.log(rep(input, env));
+        } catch (err) {
+            console.log(err);
+        } finally {
+            repl(env);
+        }
+    });
+};
+
+repl(CORE_ENV);
